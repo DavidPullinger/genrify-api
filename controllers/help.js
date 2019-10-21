143a80e00f41628a78f52ce4abbbc17bd76f349b
DavidPullinger <davidpullinger003@gmail.com>
2019-10-21 17:33:01 +0200

    Added commentsgit add .

--- controllers/help.js
+++ controllers/help.js
@@ -1,7 +1,8 @@
+// called when a user wants to add a new question
 const addHelp = (req, res, db) => {
     let { email, question } = req.body;
 
-    // insert into 
+    // insert into help table and return the "email" field
     db.insert({
         email: email,
         question: question
@@ -10,8 +11,9 @@ const addHelp = (req, res, db) => {
         .returning('email')
         .then(res.json("Success"))
 }
-
+// called when the help screen is loaded
 const getHelp = (req, res, db) => {
+    // select from help table, where the answer is NOT NULL (i.e. it has an answer). Return the helpid, question and answer
     db.select('helpid', 'question', 'answer')
         .from('help')
         .whereNot({ answer: null })
--- controllers/signup.js
+++ controllers/signup.js
@@ -1,14 +1,19 @@
+// called when a user signs up
 const handleSignUp = (req, res, db, bcrypt) => {
     const { firstName, lastName, email, dateOfBirth, gender, dateJoined, password } = req.body;
+    // hash the users password for storage
     const hash = bcrypt.hashSync(password);
 
+
     db.transaction(trx => {
+        // insert into login table, the users email and hash
         trx.insert({
             email: email,
             hash: hash
         })
             .into('login')
             .returning('email')
+            // if it is successfull, insert into the users table, their other details
             .then(loginEmail => {
                 return trx('users')
                     .returning('*')
@@ -19,12 +24,13 @@ const handleSignUp = (req, res, db, bcrypt) => {
                         dateofbirth: dateOfBirth,
                         gender: gender,
                         datejoined: dateJoined
-                    }).then(user => {
-                        res.json(user[0]);
+                    }).then(user => { 
+                        res.json(user[0]); // return the user to front end so their details can be used from there
                     })
 
             })
             .then(trx.commit)
+            // if there is an error, rollback all changes
             .catch(trx.rollback)
     })
         .catch(err => res.status(400).json('Failed to sign up'));
